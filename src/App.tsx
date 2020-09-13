8f70437aa7cf877191c65049bff0c07ce377c8c1
Emerick Bosch <xpc.dev@gmail.com>
2020-09-13 22:55:01 +0200

    Add basic layout and toolbar
    No real functionality yet.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { Banks, InputFileTypes } from "bank-schema-parser";
 
 function App() {
+  const banks = Object.values(Banks);
+  const fileTypes = Object.values(InputFileTypes);
+
+  const [selectedBank, setSelectedBank] = useState(Banks.StandardBank);
+  const [selectedFileType, setSelectedFileType] = useState(
+    InputFileTypes.Default
+  );
+
+  const bankSelect = (
+    <label>
+      Bank
+      <select
+        className="bg-gray-300 mx-4"
+        value={selectedBank}
+        onChange={(change) => setSelectedBank(change.target.value as Banks)}
+      >
+        {banks.map((bank) => (
+          <option key={bank} value={bank}>
+            {bank}
+          </option>
+        ))}
+      </select>
+    </label>
+  );
+
+  const inputFileTypeSelect = (
+    <label>
+      Input file type
+      <select
+        className="bg-gray-300 mx-4"
+        value={selectedFileType}
+        onChange={(change) =>
+          setSelectedFileType(change.target.value as InputFileTypes)
+        }
+      >
+        {fileTypes.map((type) => (
+          <option key={type} value={type}>
+            {type}
+          </option>
+        ))}
+      </select>
+    </label>
+  );
+
+  const toolBar = (
+    <div className="container flex p-4 bg-gray-100">
+      <div>
+        <h2 className="text-2xl">Import data</h2>
+        <form className="flex flex-col">
+          {bankSelect}
+          {inputFileTypeSelect}
+          <div className="flex mt-2">
+            <button type="submit" className="bg-blue-200 p-1 flex-1">
+              Import
+            </button>
+            <div className="flex-1"></div>
+            <div className="flex-1"></div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="bg-gray-700 text-gray-300 text-center">
+        <h1 className="text-4xl">bank-schema UI</h1>
       </header>
+      <div className="flex" style={{ height: "100%" }}>
+        <div className="container flex">Display stuff</div>
+        {toolBar}
+      </div>
     </div>
   );
 }
