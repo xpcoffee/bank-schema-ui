c8823d73ef193efc9525369a20200432a9e3ac04
Emerick Bosch <xpc.dev@gmail.com>
2020-09-14 21:03:51 +0200

    Add ability to parse files selected files

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,86 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useReducer } from "react";
 import "./App.css";
-import { Banks, InputFileTypes } from "bank-schema-parser";
+import {
+  Banks,
+  InputFileTypes,
+  parseFromString,
+  Transaction,
+} from "bank-schema-parser";
 import { useDropzone } from "react-dropzone";
 
 function App() {
   const banks = Object.values(Banks);
   const fileTypes = Object.values(InputFileTypes);
 
-  const [selectedBank, setSelectedBank] = useState(Banks.StandardBank);
-  const [selectedFileType, setSelectedFileType] = useState(
-    InputFileTypes.Default
-  );
-  const [selectedFile, setSelectedFile] = useState<File | undefined>();
+  type InfoLogEvent = { isoTimestamp: string; source: string; message: string };
+  type Action =
+    | {
+        type: "add";
+        transactions: Transaction[];
+        source: string;
+        eventLogs: InfoLogEvent[];
+      }
+    | { type: "reset" }
+    | { type: "selectBank"; bank: Banks }
+    | { type: "selectFileType"; inputFileType: InputFileTypes }
+    | { type: "selectFiles"; files: File[] }
+    | { type: "clearSelectedFiles" };
+  type TransactionMap = Record<string, Transaction>;
+  type State = {
+    transactions: TransactionMap;
+    eventLog: InfoLogEvent[];
+    inputFileBank: Banks;
+    inputFileType: InputFileTypes;
+    selectedFiles: File[] | undefined;
+  };
+  const initialState: State = {
+    transactions: {},
+    eventLog: [],
+    inputFileBank: Banks.StandardBank,
+    inputFileType: InputFileTypes.Default,
+    selectedFiles: undefined,
+  };
+
+  function reducer(state: State, action: Action): State {
+    switch (action.type) {
+      case "reset":
+        return initialState;
+      case "add":
+        const newState = { ...state };
+
+        action.transactions.reduce((state, transaction) => {
+          state.transactions[transaction.hash] = transaction;
+          return state;
+        }, newState);
+
+        newState.eventLog = [...newState.eventLog, ...action.eventLogs];
+        return newState;
+
+      case "selectBank":
+        return { ...state, inputFileBank: action.bank };
+
+      case "selectFileType":
+        return { ...state, inputFileType: action.inputFileType };
+
+      case "selectFiles":
+        return { ...state, selectedFiles: action.files };
+
+      case "clearSelectedFiles":
+        return { ...state, selectedFiles: undefined };
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const bankSelect = (
     <label>
       Bank
       <select
         className="bg-gray-300 mx-4"
-        value={selectedBank}
-        onChange={(change) => setSelectedBank(change.target.value as Banks)}
+        value={state.inputFileBank}
+        onChange={(change) =>
+          dispatch({ type: "selectBank", bank: change.target.value as Banks })
+        }
       >
         {banks.map((bank) => (
           <option key={bank} value={bank}>
@@ -35,9 +96,12 @@ function App() {
       Input file type
       <select
         className="bg-gray-300 mx-4"
-        value={selectedFileType}
+        value={state.inputFileType}
         onChange={(change) =>
-          setSelectedFileType(change.target.value as InputFileTypes)
+          dispatch({
+            type: "selectFileType",
+            inputFileType: change.target.value as InputFileTypes,
+          })
         }
       >
         {fileTypes.map((type) => (
@@ -49,29 +113,59 @@ function App() {
     </label>
   );
 
-  const readFile = useCallback(() => {
-    if (!selectedFile) {
+  function parsingErrorsToInfoLogEvent(
+    source: string,
+    errors: string[]
+  ): InfoLogEvent[] {
+    const isoTimestamp = new Date(Date.now()).toISOString();
+    return errors.map((message) => {
+      return { isoTimestamp, source, message };
+    });
+  }
+
+  const processFileContents = useCallback(
+    (fileName: string, contents: string) => {
+      parseFromString({
+        bank: state.inputFileBank,
+        type: state.inputFileType,
+        inputString: contents,
+        deduplicateTransactions: true,
+      }).then((statement) => {
+        dispatch({
+          type: "add",
+          transactions: statement.transactions,
+          source: fileName,
+          eventLogs: parsingErrorsToInfoLogEvent(
+            fileName,
+            statement.parsingErrors
+          ),
+        });
+      });
+    },
+    [state.inputFileBank, state.inputFileType]
+  );
+
+  const readFiles = useCallback(() => {
+    if (!state.selectedFiles) {
       console.error("No file selected. Please select a file first.");
       return;
     }
 
-    const reader = new FileReader();
-    reader.onabort = () => console.log("file reading was aborted");
-    reader.onerror = () => console.log("file reading has failed");
-    reader.onload = () => {
-      // Do whatever you want with the file contents
-      const binaryStr = reader.result;
-      console.log(binaryStr);
-    };
-    reader.readAsArrayBuffer(selectedFile);
-  }, [selectedFile]);
+    state.selectedFiles.forEach((file) => {
+      const reader = new FileReader();
+      reader.onabort = () => console.log("file reading was aborted");
+      reader.onerror = () => console.log("file reading has failed");
+      reader.onload = () =>
+        processFileContents(file.name, reader.result as string);
+      reader.readAsText(file);
+    });
+  }, [processFileContents, state.selectedFiles]);
 
   const onDrop = useCallback((acceptedFiles: File[]) => {
     if (acceptedFiles.length !== 1) {
       console.log("Please only select a single file.");
     }
-
-    setSelectedFile(acceptedFiles[0]);
+    dispatch({ type: "selectFiles", files: acceptedFiles });
   }, []);
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
@@ -100,7 +194,7 @@ function App() {
           style={{ height: "100%" }}
           onSubmit={(e) => {
             e.preventDefault();
-            readFile();
+            readFiles();
           }}
         >
           {bankSelect}
@@ -125,7 +219,10 @@ function App() {
       </header>
       <div className="flex justify-between" style={{ height: "100%" }}>
         <div className="flex-grow p-2" style={{ width: "100%" }}>
-          Display stuff
+          <h2 className="text-2xl">Transactions</h2>
+          <pre>{JSON.stringify(Object.values(state.transactions))}</pre>
+          <h2 className="text-2xl">Event log</h2>
+          <pre>{JSON.stringify(state.eventLog)}</pre>
         </div>
         {toolBar}
       </div>
